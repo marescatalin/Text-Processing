d88ec14cc90d1b5ad4ea3cc9247d0114c90e3a6c
Catalin Mares <catalinmares@pc-206-18.customer.aspect3.aqh.lan>
2018-11-04 23:23:44 +0000

    refactored code for more efficiency

--- my_retriever.py
+++ my_retriever.py
@@ -4,26 +4,23 @@ class Retrieve:
     def __init__(self,index, termWeighting):
         self.index = index
         self.termWeighting = termWeighting
-        self.maximumArticles = 0
+        self.maximumArticles = self.maxArticles()
+        self.idfScore = self.computeIDF()
 
-        maximumArticles = 0
-        for word in self.index.keys():
-            for article in self.index[word]:
-                if article > self.maximumArticles:
-                    self.maximumArticles = article
 
     # Method performing retrieval for specified query
     def forQuery(self, query):
-        bestArticles, articleMatrix = [], ([0] * (self.maximumArticles + 1))
+        #Declare variables to be used in the program, adding 1 so that the index matches the articleID
+        bestArticles, articleMatrix, frequencySum = [], ([0] * (self.maximumArticles + 1)), ([0] * (self.maximumArticles + 1))
 
 
         if (self.termWeighting == 'binary'):
+            #Compute binary weight of each term for each article
             for key in query.keys():
                 if key in self.index:
                     for article,count in self.index[key].items(): articleMatrix[article] += 1
 
-            frequencySum = ([0] * (self.maximumArticles + 1))
-
+            #Compute vector size
             for word in self.index.keys():
                 for article, frequency in self.index[word].items():
                     frequencySum[article] += math.pow(1, 2)
@@ -33,14 +30,8 @@ class Retrieve:
 
             # Normalise the tf computed for each article
             for number in range(1, self.maximumArticles): articleMatrix[number] = articleMatrix[number] / sqrFrequencySum[number]
-
-
-            for x in range(10):
-                indexOfMaxValue = articleMatrix.index(max(articleMatrix))
-                bestArticles.append(indexOfMaxValue)
-                articleMatrix[indexOfMaxValue] = 0
-
-            return bestArticles
+            # return top articles
+            return self.topArticles(articleMatrix)
 
 
         elif (self.termWeighting == 'tf'):
@@ -52,8 +43,6 @@ class Retrieve:
                         articleMatrix[article] += frequency*query[key]
 
         #Computing the square root of the square sum of the frequency of each term for each article
-            frequencySum = ([0] * (self.maximumArticles + 1))
-
             for word in self.index.keys():
                 for article, frequency in self.index[word].items():
                     frequencySum[article] += math.pow(frequency, 2)
@@ -64,60 +53,55 @@ class Retrieve:
 
         #Normalise the tf computed for each article
             for number in range(1, self.maximumArticles): articleMatrix[number] = articleMatrix[number] / sqrFrequencySum[number]
-
-
-            for x in range(10):
-                indexOfMaxValue = articleMatrix.index( max(articleMatrix))
-                bestArticles.append(indexOfMaxValue)
-                articleMatrix[indexOfMaxValue] = 0
-
-            return bestArticles
-
-
-
-
+            # return top articles
+            return self.topArticles(articleMatrix)
 
         elif (self.termWeighting == 'tfidf'):
 
-            idfScore = {}
-            #Compute the total number of documents / term occurance in the data set
-            for term in self.index:
-                idfScore[term] = math.log10(self.maximumArticles/len(self.index[term].items()))
+            # Compute the total number of documents / term occurance in the data set
+
+
 
                 # Computing the terms that are both in the query and article
             for key in query.keys():
                 if key in self.index:
                     for article, frequency in self.index[key].items():
-                            articleMatrix[article] += (frequency * idfScore[key])
+                            articleMatrix[article] += (frequency * self.idfScore[key])
 
                 # Computing the square root of the square sum of the frequency of each term for each article
-            frequencySum = ([0] * (self.maximumArticles + 1))
-
             for word in self.index.keys():
                 for article, frequency in self.index[word].items():
-                    frequencySum[article] += math.pow(frequency*(idfScore[word]), 2)
+                    frequencySum[article] += math.pow(frequency*(self.idfScore[word]), 2)
 
                 # Square root the sum for each article
             sqrFrequencySum = [math.sqrt(x) for x in frequencySum]
 
                 # Normalise the tf computed for each article
             for number in range(1, self.maximumArticles): articleMatrix[number] = articleMatrix[number] / sqrFrequencySum[number]
+            # return top articles
+            return self.topArticles(articleMatrix)
 
+    #Extract the indexes of the top 10 scoring articles
+    def topArticles(self,articleMatrix):
+        bestArticles = []
+        for x in range(10):
+            indexOfMaxValue = articleMatrix.index(max(articleMatrix))
+            bestArticles.append(articleMatrix.index(max(articleMatrix)))
+            articleMatrix[indexOfMaxValue] = 0
+        return bestArticles
 
-            for x in range(10):
-                indexOfMaxValue = articleMatrix.index(max(articleMatrix))
-                bestArticles.append(indexOfMaxValue)
-                articleMatrix[indexOfMaxValue] = 0
+    #Comnpute the maximum number of articles in the set
+    def maxArticles(self):
+        maximumArticles = 0
+        for word in self.index.keys():
+            for article in self.index[word]:
+                if article > maximumArticles:
+                    maximumArticles = article
+        return maximumArticles
 
-            return bestArticles
-
-
-
-
-
-
-
-
-
-
+    def computeIDF(self):
+        idfScore = {}
+        for term in self.index:
+            idfScore[term] = math.log10(self.maximumArticles/len(self.index[term].items()))
+        return idfScore
 
